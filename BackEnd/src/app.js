9b92c796b579f7a610ca2f998bc9c64f7b495ec0
Hitesh <hitesh.parashar2016@gmail.com>
2025-11-09 01:49:24 +0530

    add CORS support

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import aiRoutes from './routes/ai.routes.js';
+import cors from 'cors';
 
 const app = express()
+app.use(cors())
 
 app.use(express.json());
 app.get('/', (req, res) => {
