9308194ea43d74e2e2f15285fe29d7e09557b89e
hitesh-2357 <61927995+hitesh-star@users.noreply.github.com>
2025-11-12 01:01:25 +0530

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
   })
 
   async function reviewCode(){
-    const response=await axios.post("http://localhost:3000/ai/get-review",{code})
+    const response=await axios.post("https://code-ai-reviewer-backend.onrender.com/ai/get-review",{code})
     console.log(response.data);
     setReview(response.data);
   }
